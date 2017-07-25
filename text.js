e3cab88b6c481804b952273354913fbe23d3a6ec
r11n <raghavendra.n@greycampus.com>
2017-07-25 12:07:04 +0530

    added programs

--- text.js
+++ text.js
@@ -1,1 +1,18 @@
- console.log(5 + 6);
+function prompt(question, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.once('data', function (data) {
+        callback(data.toString().trim());
+    });
+}
+for (var i = 0; i < 3; i++) {
+  prompt('Whats your name?', function (input) {
+      console.log(input);
+      process.exit();
+  });
+
+}
